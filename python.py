618c2bb1ea9f2130e5b49eff0e8b90853b52e78c
PrinceAkram <byiringiroprince003@gmail.com>
2026-06-25 21:30:37 +0200

    Add telecommunication company records script
    Initialize a script to calculate average call duration.

---
+++ python.py
@@ -0,0 +1,10 @@
+#----------1.TELECOMMUNICATION COMPANY RECORDS--------------
+avrg=[]
+calls=[7,8,9,10,11,12,13,14,15,16]
+a=len(calls)
+b=sum(calls)
+print("length of calls:", len(calls))
+print("sum of calls:", sum(calls))
+avrg(calls)=float(a/b)
+print("sum(calls)/len(calls):",avrg(calls))
+
